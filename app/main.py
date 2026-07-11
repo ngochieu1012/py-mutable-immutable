404518d7667b5fb63cfe49578b78a2d7404a650e
Hieu Ngoc <Ngoc Hieu>
2026-07-11 21:47:52 +0700

    Fix flake8 formatting errors-3

--- app/main.py
+++ app/main.py
@@ -17,9 +17,11 @@ marks = {
 collection_of_coins = {1, 2, 25}
 # write your code here
 # Get all variables from main module
-all_vars = {k: v for k, v in globals().items()
-            if not k.startswith("_")
-            and k not in ("all_vars", "mutable", "immutable", "sorted_variables")}
+all_vars = {
+    k: v
+    for k, v in globals().items()
+    if not k.startswith("_") and k not in excluded
+}
 # Separate mutable and immutable
 mutable = []
 immutable = []
